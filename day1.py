d7251c5d72e9dcb7a5dd45fc1b66bdc1f80ef4c7
oz <austinj.tech@gmail.com>
2022-12-01 19:16:53 -0600

    Add files via upload

--- day1.py
+++ day1.py
@@ -1,14 +1,8 @@
-f = sorted(eval(open("input.txt").read().
-    replace('\n\n', ',').replace('\n', '+')))
+var = open(0,'r').read().split("\n\n")
+total_kcal = []
 
-print(f[-1], sum(f[-3:]))
+for line in var:
+    total_kcal.append(sum([int(i) for i in line.split("\n")]))
 
-
-#
-# 
-# var = open('input.txt','r').read().split("\n\n")
-#total_kcal = []
-
-#for line in var:
-#    total_kcal.append(sum([int(i) for i in line.split("\n")]))
-#print(max(total_kcal))
+print(max(total_kcal))
+print(sum(sorted((total_kcal))[-3:]))
